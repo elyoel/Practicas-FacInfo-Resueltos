a1cfdf72bb2addd981203552f53fc8039596571d
Yoel <133163866+elyoel@users.noreply.github.com>
2025-08-30 21:16:57 -0300

    actividad4

--- Taller/Modulo Imperativo/actividad 4.pas
+++ Taller/Modulo Imperativo/actividad 4.pas
@@ -15,11 +15,153 @@ f. Calcule el promedio de los precios del vector resultante del punto d).}
 
 program actividad4;
 const
+    maxRubro = 6;
+    dimF = 30;
 type
+    rubros: 1..maxRubro;
+    producto = Record
+        codigo: Integer;
+        cod_rubro: rubros
+        precio: Real;
+    end;
 
+    lista: ^nodo;
+    nodo  = Record
+        elem:  producto;
+        sig: lista;
+    end;
 
+    v_lista  = array[rubros] of lista;
+    v_producto  = array[1..dimF] of producto;
+
+procedure insertar_ordenado(var ls: lista; elem: producto);
+var
+    ant, act, nuevo: lista;
+begin
+    new(nuevo);
+    nuevo^:= elem
+    act:= ls, ant;= ls;
+    While (act <> Nil) AND (elem.codigo > act^.elem.codigo) do
+    begin
+        ant:= act;
+        act:= act^.sig;
+    end;
+    If (act = ant) then
+        ls:= nuevo;
+    Else
+        ant^.sig := nuevo;
+    nuevo^.sig:= act;
+end;
+
+procedure cargar_lista(v: v_lista);
+    procedure inicializar_vector(var v: v_lista);
+    var i: rubros;
+    begin
+        for i:= 1 to maxRubro do
+            v[i]:= Nil;
+    end;
+    procedure leer_producto(var p: producto);
+    begin
+        WriteLn('Ingrese precio:');
+        ReadLn(p.precio);
+        If (p.precio <> -1); then
+        begin
+        WriteLn('Ingrese Codigo:');
+        ReadLn(p.codigo);
+        WriteLn('Ingrese Codigo de rubro:');
+        ReadLn(p.cod_rubro);
+        end;
+    end;
+var
+    i: rubros;
+    p: producto;
+begin
+    inicializar_vector(v);
+    leer_producto(p);
+    While (p.precio <> -1) do
+    begin
+        insertar_ordenado(v[p.rubro], p);
+        leer_producto(p);
+    end;
+end;
+
+procedure imprimir_codigo_por_rubro(v: v_lista);
+    procedure imprimir_codigos(ls: lista);
+    begin
+        While (ls <> Nil) do
+        begin
+            WriteLn('Producto con Codigo: ', ls^.elem.codigo);
+            ls:= ls^.sig
+        end;
+    end;
+var i: rubros;
+begin
+    for i:= 1  to maxRubro do
+    begin
+        WriteLn('Los codigos del Rubro: ', i);
+        WriteLn('Son: ')
+        imprimir_codigos(v[i]);
+    end;
+end;
+
+procedure generar_vector(var v: v_producto; var dimL: Integer; ls: lista);
+var 
+begin
+    dimL:= 0;
+    While (ls <> Nil) AND (dimL <= dimF) do
+    begin
+        dimL:= dimL + 1;
+        v[dimL]:= ls^.elem;
+        ls:= ls^.sig;
+    end
+end;
+
+procedure ordenar_vector(var v: v_producto; dimL: Integer);
+var
+    i, j: integer
+    actual: producto;
+begin
+    For i:= 2 to dimL do
+    begin
+        actual:= v[i];
+        j:= i-1;
+        While (j > 0) AND (v[j] > actual.precio) do
+        begin
+            v[j+1]:= v[j];
+            j:= j - 1;
+        end;
+        v[j+1]:= actual;
+    end;
+end;
+
+procedure mostrar_vector(v: v_producto; dimL);
+var i: Integer;
+begin
+    WriteLn('Los precios del vector resultante: ')
+    For i:= 1 to dimL do
+        WhileLn(v[i].precio);
+end;
+
+function promediar(v: v_producto, dimL: Integer):Real;
+var 
+    i:= Integer
+    total:= Real;
+begin
+    total:= 0;
+    For i:= 1 to dimL do
+        total:= total + v[i].precio;
+    promediar:= (total/dimL)*100;
+end;
 
 var
+    vector_listas: v_lista;
+    vector_productos: v_producto;
+    dimL: Integer;
 begin
-    
+    cargar_lista(vector_listas);
+    imprimir_codigo_por_rubro(vector_listas[3]);
+    generar_vector(vector_productos, dimL, vector_listas);
+    ordenar_vector(vector_productos, dimL);
+    mostrar_vector(vector_productos, dimL);
+    WriteLn('El promedio de los precios del vector resultante', promediar(vector_productos, dimL));
 end.
