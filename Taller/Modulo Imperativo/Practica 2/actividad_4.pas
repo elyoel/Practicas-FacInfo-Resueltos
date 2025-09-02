a87b7b11ce30a077ea8b6797e772ffc66055089c
Yoel <133163866+elyoel@users.noreply.github.com>
2025-09-02 08:59:13 -0300

    actividad 4

--- Taller/Modulo Imperativo/Practica 2/actividad_4.pas
+++ Taller/Modulo Imperativo/Practica 2/actividad_4.pas
@@ -1,1 +1,27 @@
-program avtividad_4;
+program actividad_4;
+procedure ImprimirRecursivo(n: integer);
+begin
+  If (n <> 0) then
+  begin
+    ImprimirRecursivo(n DIV 2);
+    Write(n MOD 2);
+  end;
+end;
+procedure leerValor();
+var
+  num: integer;
+begin
+  WriteLn('Ingrese valor: ');
+  Read(num);
+  While (num <> 0) do
+  begin
+    WriteLn('El numero ', num, ' en binario es: ');
+    ImprimirRecursivo(num);
+    WriteLn;
+    WriteLn('Ingrese valor: ');
+    Read(num);
+  end;
+end;
+begin
+  leerValor();
+end.
