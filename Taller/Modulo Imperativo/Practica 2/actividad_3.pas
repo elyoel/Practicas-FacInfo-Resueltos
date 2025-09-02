b5b1d9b279f93dc58726f94153c5ba0329a0ba30
Yoel <133163866+elyoel@users.noreply.github.com>
2025-09-02 09:27:14 -0300

    practica 3 completada

--- Taller/Modulo Imperativo/Practica 2/actividad_3.pas
+++ Taller/Modulo Imperativo/Practica 2/actividad_3.pas
@@ -1,1 +1,35 @@
-program actividad_3;
+program actividad_2;
+const
+  max = 1550; min = 300;
+  dimF = 20;
+  
+type
+  vector = array[1..dimF];
+procedure cargarRecursivo(var v: vector; var dimL: integer);
+  function numRandom():integer;
+  begin
+    Randomize;
+    numRandom:= min + ((max - min) + 1);
+  end;
+
+var
+begin
+  If (dimL > dimF) then
+  begin
+      dimL:= dimL + 1;
+      v[dimL]:= numRandom();
+  end;
+end;
+
+procedure ordenar(var v: vector; dimL: integer);
+var
+  i, j, valor: integer;}
+begin
+  for i:= 2 to dimL do
+  begin
+    a
+  end; 
+end;
+begin
+
+end.
